abc412c9833463cb17b2cf302d9d2e53fc1a8f8e
andyanhkali174 <andyanhkali174@kali-andy.andyanh>
2024-01-29 08:33:15 +0700

    change js

--- js/configClock/displayClock.js
+++ js/configClock/displayClock.js
@@ -1,41 +1,19 @@
-function displayClock() {
-    // ... (Các phần khác giữ nguyên)
+var fuT = new Date("February  9,2024 00:00:00").getTime()
+        setInterval(function(){
+            var noW = new Date().getTime()
+            var D = fuT - noW
+            var days = Math.floor(D/(1000*60*60*24))
+            var hours = Math.floor(D/(1000*60*60))
+            var minutes = Math.floor(D/(1000*60))
+            var seconds= Math.floor(D/(1000))
 
-    // Cập nhật thời gian hiện tại và đếm ngược đến ngày cụ thể
-    this.updateTime = function () {
-        const currentDate = new Date();
-        const targetDate = new Date(2024, 0, 29, 0, 0, 0, 0); // Ngày cụ thể (29-1-2024)
+            hours %= 24
+            minutes %= 60
+            seconds %= 60
 
-        const timeDiff = targetDate - currentDate;
-        const daysRemaining = Math.floor(timeDiff / (24 * 60 * 60 * 1000));
-        const hoursRemaining = Math.floor((timeDiff % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
-        const minutesRemaining = Math.floor((timeDiff % (60 * 60 * 1000)) / (60 * 1000));
-        const secondsRemaining = Math.floor((timeDiff % (60 * 1000)) / 1000);
+            document.getElementById("days").innerText = days 
+            document.getElementById("hours").innerText = hours
+            document.getElementById("minutes").innerText = minutes 
+            document.getElementById("seconds").innerText = seconds 
 
-        // Update text content for element-time
-        this.elementTime[0].innerText = this.formatTimeUnit(hoursRemaining);
-        this.elementTime[1].innerText = this.formatTimeUnit(minutesRemaining);
-        this.elementTime[2].innerText = this.formatTimeUnit(secondsRemaining);
-
-        // Update text content for date (ngày, tháng, năm)
-        this.elementTime[3].innerText = this.formatTimeUnit(daysRemaining);
-        this.elementTime[4].innerText = this.formatTimeUnit(currentDate.getMonth() + 1); // Tháng bắt đầu từ 0
-        this.elementTime[5].innerText = currentDate.getFullYear();
-    };
-
-    // ... (Các phần khác giữ nguyên)
-}
-
-// ... (Các phần khác giữ nguyên)
-
-// Khởi chạy các hàm
-var liveClock = new displayClock();
-liveClock.displayContainClock();
-liveClock.displayElementTime();
-liveClock.displayTimeName();
-liveClock.displayHaiCham();
-
-// Cập nhật thời gian mỗi giây
-setInterval(() => {
-    liveClock.updateTime();
-}, 1000);
+        },1000) 
